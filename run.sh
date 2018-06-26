2d9e57261c3cb1be30c979da686b4bd3a9da1efe
Johannes Filter <hi@jfilter.de>
2018-06-26 21:13:05 +0200

    exit script if it is already running

--- run.sh
+++ run.sh
@@ -2,6 +2,11 @@
 set -e
 
 while : ; do
-  /home/jfilter/.local/bin/trackthenews /home/jfilter/code/ifg-feed/ttnconfig/
-  sleep 30
+  if pgrep -f "trackthenewsy" &>/dev/null; then
+      echo "trackthenews is already running. exiting."
+      exit
+  else
+    /home/jfilter/.local/bin/trackthenews /home/jfilter/code/ifg-feed/ttnconfig/
+    sleep 30
+  fi
 done &
